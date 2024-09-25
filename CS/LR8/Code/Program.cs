40ae71af3c0cc94db6908c7f91b069926d5943d5
Archont <144602015+Archont8@users.noreply.github.com>
2024-09-25 20:37:30 +0300

    CS_LR8_Done

--- CS/LR8/Code/Program.cs
+++ CS/LR8/Code/Program.cs
@@ -1,19 +1,19 @@
 ﻿using Microsoft.Data.Sqlite;
 
-string readText = File.ReadAllText("Import.txt");
-var listStrLineElements = new List<string>();
-listStrLineElements = readText.Split(new string[] { Environment.NewLine }, StringSplitOptions.None).ToList();
-List<string> rowList = listStrLineElements.SelectMany(s => s.Split('\t')).ToList();
+string[] allLines = File.ReadAllLines("Import.txt");
+
+string[] neededLines = allLines.ToArray();
+
 string con = "Data Source = db2.db";
 using (var connect = new SqliteConnection(con)) {
-    // Соединяемся с DB
     connect.Open();
     var command = connect.CreateCommand();
-    for (int i = 0; i < 5; ++i) {
-        SqliteParameter Name = new SqliteParameter("$Name", rowList[i]);
-        command.Parameters.Add(Name);
-        command.CommandText = @"
-        insert into Users (Name) values ($Name)
+    foreach (var x in neededLines.Select((value, index) => new {value, index})) {
+        command.Parameters.AddWithValue("@UserID", x.index);
+        command.Parameters.AddWithValue("@Name", x.value);
+        command.CommandText = @$"
+        insert into Users(UserID, Name) 
+        values (@UserID, @Name)
         ";
     }
     command.ExecuteNonQuery();
--- CS/LR8/Code/obj/Debug/net8.0/Code.AssemblyInfo.cs
+++ CS/LR8/Code/obj/Debug/net8.0/Code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b43d8290c88ed347407479bec002f06dc0fdcb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4eec9d4ac04ff0d2d9a0ef970e43636c1519868")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
