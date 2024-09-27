56af208bd877abfd1c16cd6fddd4153b01840e9e
Archont <archontspam@mail.ru>
2024-09-27 17:36:18 +0300

    LR8_AlmostDone

--- CS/LR9/Code/Program.cs
+++ CS/LR9/Code/Program.cs
@@ -1,43 +1,67 @@
 ﻿using Microsoft.Data.Sqlite;
-using static System.Console;
 using System.Data;
+using static System.Console;
 class Program {
     static void Main() {
-        WriteLine("Enter a path to a database:");
+        Info();
+    }
+    static public void Info() {
+        Write("Enter a path to a database:");
         string path = ReadLine()!;
         string connection = $"Data source = {path}";
         using (var connect = new SqliteConnection(connection)) {
             connect.Open();
             var command = new SqliteCommand();
             command.Connection = connect;
-            WriteLine("Database has been created\nWould you like to create a table? y/n");
-            string ChoiseToCreateATable = ReadLine()!;
-            int CountOfTables;
-            if (ChoiseToCreateATable == "y") {
-                WriteLine("How many tables would you like to create?");
-                CountOfTables = Convert.ToInt32(ReadLine()!);
-            }   
-            if (ChoiseToCreateATable == "n") {
-                WriteLine(@"So what do you want to do?\n
-                1. See the data of a particular table\n
-                2. Create a table\n
-                3. Insert the data to a table
-                ");
-            }
+            WriteLine("Database has been created\n");
+            Selection(command);
             command.CommandText = "";
         }
     }
-    public void ShowData() {}
-    public void CreateNewTable(SqliteCommand com, List<string> TableColumns) {
+    static public void Selection(SqliteCommand com) {
+        WriteLine(@"What do you want to do?
+        1. Create a table
+        2. Show data
+        3. Insert row
+        4. Exit
+        ");
+        var TableColumns = new Dictionary<string, string>();
+        int Selection = Convert.ToInt32(ReadLine());
+        switch (Selection) {
+            case 1: ShowData(com, TableColumns); break;
+            case 2: CreateNewTable(com, TableColumns); break;
+            case 3: InsertDataToATable(com, TableColumns); break;
+            case 4: Environment.Exit(0); break;
+        }
+    }
+    static public void ShowData(SqliteCommand com, Dictionary<string, string> tableColumns) {
+        WriteLine("Select the table to show the data");
+        string TableName = ReadLine()!;
+        using (SqliteDataReader reader = com.ExecuteReader())
+        {
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    foreach (var x in tableColumns.Select((value, index) => new {value, index})) {
+                    string NameOfColumn = x.value.Key.ToString();
+                    var valueOfColumn = reader.GetValue(x.index);
+                    Console.WriteLine($"{valueOfColumn}");
+                    }
+                }
+            }
+        }
+    }
+    static public void CreateNewTable(SqliteCommand com, Dictionary<string, string> tableColumns) {
         WriteLine("Enter a name of the new table: ");
         string NameOfNewTable = ReadLine()!;
         WriteLine("Enter the quantity of table columns: ");
         int QuantityOfColumns = Convert.ToInt32(ReadLine());
-        WriteLine($"Enter a name of the field 1: ");
+        WriteLine($"Enter a name of the column 1: ");
         string FirstColumnName = ReadLine()!;
-        TableColumns.Add(FirstColumnName);
-        WriteLine($"Enter a type of the field 1: ");
+        WriteLine($"Enter a type of the column 1: ");
         string FirstColumnType =  ReadLine()!;
+        tableColumns.Add(FirstColumnName, FirstColumnType);
         com.CommandText = @$"
         create table {NameOfNewTable} (
         {FirstColumnName} {FirstColumnType}
@@ -50,28 +74,23 @@ class Program {
                 column = new DataColumn();
                 WriteLine($"Enter a name of the field {i}: ");
                 string NameOfNewColumn = ReadLine()!;
-                TableColumns.Add(NameOfNewColumn);
                 WriteLine($"Enter a type of the field {i}: ");
                 string TypeOfNewColumn = ReadLine()!;
+                tableColumns.Add(NameOfNewColumn, TypeOfNewColumn);
                 column.DataType = Type.GetType(TypeOfNewColumn);
                 column.ColumnName = NameOfNewColumn;
                 dt.Columns.Add(column);
-                WriteLine($"\nThe column '{NameOfNewColumn}' has been created.\n");
             }
         }
     }
-    /*public void InsertDataToATable(SqliteCommand com, 
-    string[] TableFields, string[] Values) {
-        if (TableFields.Length == Values.Length) {
-            WriteLine("Enter the table name");
-            string TableName = ReadLine()!;
-            for (int i = 0; i < TableFields.Length; ++i) {
+    static public void InsertDataToATable(SqliteCommand com, Dictionary<string, string> tableColumns) {
+        WriteLine("Select the table to insert the row into");
+        string TableName = ReadLine()!;
+            foreach (var x in tableColumns) {
                 com.CommandText = @$"
-                insert into {TableName}({TableFields[i]})
-                values ({Values[i]})
+                insert into {TableName}({x.Key})
+                values ({x.Value})
                 ";
             }
-        }
-    }*/
+    }
 }
-//INSERT INTO friends (Nickname, DateOfBirthday) VALUES ($Nickname, $DateOfBirthday)
