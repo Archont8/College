872e9d0f5efe36b7d12b2dac3141bdf14b7e13b5
Archont <archontspam@mail.ru>
2024-10-01 13:35:35 +0300

    Transactions2

--- CS/LR_Transactions/Code/Program.cs
+++ CS/LR_Transactions/Code/Program.cs
@@ -1,1 +1,76 @@
-﻿
+﻿using System.Diagnostics;
+using Microsoft.Data.Sqlite;
+struct Program {
+    async static Task Main(){
+        Console.Write("Enter the path: ");
+        string path = Console.ReadLine()!;
+        await using (var connect = new SqliteConnection($"Data Source = {path}")) {
+        await connect.OpenAsync();
+        string tableNumber = $@"
+        create table if not exists number(
+        ar1 int,
+        ar2 int
+        )";
+        await using (SqliteCommand command = new(tableNumber, connect)) {command.ExecuteNonQuery();}
+        //await Task.Run(()=>DBfull(connect));
+        var stopwatch= Stopwatch.StartNew();
+        await Task.Run(()=>NoTransaction(connect));
+        stopwatch.Stop();
+        Console.WriteLine(stopwatch.Elapsed.TotalMilliseconds);
+        stopwatch = Stopwatch.StartNew();
+        await Task.Run(()=>WithTransaction(connect));
+        stopwatch.Stop();
+        Console.WriteLine(stopwatch.Elapsed.TotalMilliseconds);
+        }
+    }
+    async static Task DBfull(SqliteConnection con){
+            await con.OpenAsync();
+            Parallel.For(0, 1000, async i =>
+            {
+            var command = con.CreateCommand();
+            command.CommandText = @"INSERT 
+            INTO number(ar1, ar2)
+            VALUES ($ar1, $ar2)
+            ";
+                command.Parameters.AddWithValue("$ar1",new Random().Next(1,9));
+                command.Parameters.AddWithValue("$ar2",new Random().Next(1,10000));
+                await command.ExecuteNonQueryAsync();
+                command.Parameters.Clear();
+            });
+    }
+    static async Task NoTransaction(SqliteConnection con){
+            await con.OpenAsync();
+            Parallel.For(0, 1000, (i)=>
+            {
+            var command = con.CreateCommand();
+            command.CommandText = @"
+            SELECT *
+            FROM number
+            WHERE ar2>1000
+                AND ar2<5000
+                AND ar%7==0;
+            ";
+            });
+    }
+    static async Task WithTransaction(SqliteConnection con){
+        await con.OpenAsync();
+        var transaction = con.BeginTransaction();
+        var command = con.CreateCommand();
+        command.Transaction = transaction;
+        Parallel.For(0, 1000, async i =>
+        {
+            var command = con.CreateCommand();
+            command.CommandText = @"
+            SELECT *
+            FROM number
+            WHERE ar2 > 1000
+                AND ar2 < 5000
+                AND ar2 % 7==0;
+            ";
+            await command.ExecuteNonQueryAsync();
+        });
+        await transaction.CommitAsync();
+        transaction = null;
+        command = null;
+    }
+}
