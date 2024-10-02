a85fc0116b3eac9657beee997afc21566d5ce7be
archont <144602015+Archont8@users.noreply.github.com>
2024-10-02 11:58:15 +0300

    Debugging1

--- CS/LR1/Code/Program.cs
+++ CS/LR1/Code/Program.cs
@@ -3,7 +3,7 @@ using BetterConsoles.Tables;
 using BetterConsoles.Tables.Configuration;
 
 class Recursion {
-    static ulong Rec(ulong x) {
+    static int Rec(int x) {
         if(x <= 18)
             return x + 3;
         else if(x > 18 && x % 3 == 0)
@@ -11,40 +11,44 @@ class Recursion {
         else
             return Rec(x - 1) + x * x + 5;
     }
-    static public ulong RecSum() {
-        ulong k = 0;
-        for (ulong i = 1; i <= 10000000; i++) {
+    static public int RecSum() {
+        int k = 0;
+        Parallel.For (1, 10000000, i =>
+        {
             bool ch = true;
-            ulong a = Rec(i);
-            while (a != 0) {
+            int a = Rec(i);
+            while (a != 0)
+            {
                 if (a % 2 == 0)
                     a /= 10;
-                else {
+                else
+                {
                     ch = false;
                     break;
                 }
             }
             if (ch == true) k++;
-        }
+        });
         return k;
     }
 }
 
 class Program {
-    static void Main() {
+    async static Task Main() {
         var sw = new Stopwatch();
         sw.Start();
         Recursion.RecSum();
         sw.Stop();
         long TimeNoTask = sw.ElapsedMilliseconds;
+        sw.Reset();
         sw.Start();
-        var TaskStartNew = Task.Factory.StartNew(Recursion.RecSum);
+        int resultTask = await Task.Run(Recursion.RecSum);
         sw.Stop();
         long TimeTask = sw.ElapsedMilliseconds;
         var table = new Table("Способ", "Время (мс)", "Результат");
         table.Config = TableConfig.MySql();
         table.AddRow("Без Task", TimeNoTask, Recursion.RecSum());
-        table.AddRow("С Task", TimeTask, TaskStartNew.Result);
+        table.AddRow("С Task", TimeTask, resultTask);
         Console.Write(table.ToString());
     }
 }
