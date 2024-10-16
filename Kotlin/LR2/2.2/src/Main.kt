c76932079c3868fe64684672ccfdf913a7af1bbc
Archont <144602015+Archont8@users.noreply.github.com>
2024-10-16 13:55:08 +0300

    KotlinDeleted

--- Kotlin/LR2/2.2/src/Main.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    print("x = ");
-    val x: Double = readln().toDouble();
-    print("y = ");
-    val y: Double = readln().toDouble();
-    if (x > 0 && y < 0) {
-        println("Yes, the point is situated in fourth quarter");
-    }
-}
