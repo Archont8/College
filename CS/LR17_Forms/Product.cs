bdea78cfa0a6ef034cc48ab422f785c82082d452
Archont <144602015+Archont8@users.noreply.github.com>
2024-10-28 19:09:34 +0300

    C#_DecreasedSize

--- CS/LR17_Forms/Product.cs
+++ CS/LR17_Forms/Product.cs
@@ -1,13 +1,15 @@
 ﻿using System.ComponentModel;
 
-namespace GetStartedWinForms;
-
-public class Product
+namespace Code
 {
-    public int ProductId { get; set; }
 
-    public string? Name { get; set; }
+    public class Product
+    {
+        public int ProductId { get; set; }
 
-    public int CategoryId { get; set; }
-    public virtual Category Category { get; set; } = null!;
+        public string? Name { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; } = null!;
+    }
 }
