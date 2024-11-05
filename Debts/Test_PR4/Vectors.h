70ae86924875759f19f4f70188944d33b314012d
archont <144602015+Archont8@users.noreply.github.com>
2024-11-05 10:32:15 +0300

    Test_PR4

---
+++ Debts/Test_PR4/Vectors.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef VECTORS_H
+#define VECTORS_H
+#include <vector>
+
+class Vectors
+{
+public:
+	static void Initialize();
+private:
+	static void CountOfLocalMinimums(std::vector<int>&);
+};
+#endif
