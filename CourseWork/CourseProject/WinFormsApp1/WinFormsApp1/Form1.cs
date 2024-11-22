a891957572c2b8e708da53af2dc403e02f30e28e
archont <144602015+Archont8@users.noreply.github.com>
2024-11-22 12:25:56 +0300

    CourseWorks1.1

---
+++ CourseWork/CourseProject/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System.Data.SQLite;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string connect = "Data Source = C:\\soldiers.db";
+        string pathToFile = "C:\\Soldiers";
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(pathToFile))
+            {
+                File.Create(pathToFile).Close();
+            }
+            using (var connection = new SQLiteConnection(connect))
+            {
+                connection.Open();
+                string sql = @"
+                CREATE TABLE IF NOT EXISTS Soldiers(
+                id INTEGER NOT NULL PRIMARY KEY, 
+                FIO nvarchar (50),
+                ValidityCategory nvarchar (50),
+                MilitaryBranch nvarchar (50),
+                Address nvarchar (50),
+                PassportNumber nvarchar (50),
+                PassportSeries nvarchar (50),
+                )";
+                using (var command = new SQLiteCommand(sql, connection))
+                    command.ExecuteNonQuery();
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text + " " + textBox2.Text + " " + textBox3.Text + " " + textBox4.Text + " " + textBox5.Text + " " + textBox6.Text;
+            try
+            {
+                if (textBox1.Text.Length == 0 | textBox2.Text.Length == 0 | textBox3.Text.Length == 0 | textBox4.Text.Length == 0 | textBox5.Text.Length == 0 | textBox6.Text.Length == 0)
+                {
+                    throw new Exception("One of the fields is empty!");
+                }
+                foreach (var x in textBox1.Text)
+                {
+                    if (Char.IsDigit(x))
+                    {
+                        throw new Exception("Field FIO can't contain digits");
+                    }
+                    else if (Char.IsControl(x))
+                    {
+                        throw new Exception("Field FIO can't contain symbols");
+                    }
+                }
+                if (textBox2.Text.Length > 1)
+                {
+                    throw new Exception("Field validity category can't contain more than 1 symbol");
+                }
+                foreach (var x in textBox2.Text)
+                {
+                    if (Char.IsDigit(x))
+                    {
+                        throw new Exception("Field validity category can't contain digits");
+                    }
+                    else if (Char.IsControl(x))
+                    {
+                        throw new Exception("Field validity category can't contain symbols");
+                    }
+                }
+                foreach (var x in textBox3.Text)
+                {
+                    if (Char.IsDigit(x))
+                    {
+                        throw new Exception("Field military branch can't contain digits");
+                    }
+                    else if (Char.IsControl(x))
+                    {
+                        throw new Exception("Field military branch can't contain controls");
+                    }
+                }
+                foreach (var x in textBox5.Text)
+                {
+                    if (Char.IsLetter(x))
+                    {
+                        throw new Exception("Passport number can't contain letters");
+                    }
+                    if (Char.IsControl(x))
+                    {
+                        throw new Exception("Passport number can't contain symbols");
+                    }
+                }
+                foreach (var x in textBox6.Text)
+                {
+                    if (Char.IsLetter(x))
+                    {
+                        throw new Exception("Passport series can't contain letters");
+                    }
+                    else if (Char.IsControl(x))
+                    {
+                        throw new Exception("Passport series can't contain symbols");
+                    }
+                }
+                checkedListBox1.Items.Add(text);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error! " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (checkedListBox1.Items.Count > 0)
+                {
+                    checkedListBox1.Items.Clear();
+                }
+                else
+                {
+                    throw new Exception("The list is empty, there is nothing to clear");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error! " + ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Soldier soldier = new Soldier(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text, textBox6.Text);
+
+            string sql = @"
+            INSERT INTO Soldiers(id, FIO, ValidityCategory, MilitaryBranch, Address, PassportNumber, PassportSeries)
+            SELECT max(id) + 1, @FIO, @ValidityCategory, @MilitaryBranch, @Address, @PassportNumber, @PassportSeries
+            ";
+            using (var con = new SQLiteConnection(connect))
+            {
+                using (var command = new SQLiteCommand(sql, con))
+                {
+                    command.Parameters.AddWithValue("@FIO", soldier.FIO);
+                    command.Parameters.AddWithValue("@ValidityCategory", soldier.ValidityCategory);
+                    command.Parameters.AddWithValue("@MilitaryBranch", soldier.MilitaryBranch);
+                    command.Parameters.AddWithValue("@Address", soldier.Address);
+                    command.Parameters.AddWithValue("@PassportNumber", soldier.PassportNumber);
+                    command.Parameters.AddWithValue("@PassportSeries", soldier.@PassportSeries);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Remove(checkedListBox1.SelectedItem);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter sw = File.AppendText("Soldiers"))
+            {
+                sw.WriteLine(checkedListBox1.Items.ToString());
+            }
+            MessageBox.Show("Written successfully!");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(pathToFile, "");
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Remove(checkedListBox1.SelectedItem);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Remove(checkedListBox1.SelectedItem);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Remove(checkedListBox1.SelectedItem);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Remove(checkedListBox1.SelectedItem);
+        }
+    }
+    public class NodeSorter : System.Collections.IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            TreeNode tx = x as TreeNode;
+            TreeNode ty = y as TreeNode;
+
+            // Compare the length of the strings, returning the difference.
+            if (tx.Text.Length != ty.Text.Length)
+                return tx.Text.Length - ty.Text.Length;
+
+            // If they are the same length, call Compare.
+            return string.Compare(tx.Text, ty.Text);
+        }
+    }
+}
