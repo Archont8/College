83f63a1893178464f26e63310c538d3cf56e4c16
archont <144602015+Archont8@users.noreply.github.com>
2024-12-18 10:08:02 +0300

    Test1.0

---
+++ CourseWork/CourseProject/WinFormsApp1/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b34e7c1b-1457-4e1a-af80-282890c198b0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CourseWork/CourseProject/WinFormsApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WinFormsApp1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public UnitTest1() 
+        { 
+            Form1.textBox1.Text = "Коваленко Владислав Герасимович";
+            Form1.textBox2.Text = "А";
+            Form1.textBox3.Text = "Инженерные";
+            Form1.textBox4.Text = "пос. Моревка";
+            Form1.textBox5.Text = "4223";
+            Form1.textBox6.Text = "232323";
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            button1_Click();
+            Assert.IsTrue(Form1.checkedListBox1.Length == 1);
+
+        }
+    }
+}
